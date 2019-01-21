caf6f6005645196cf289cb92cf8a9c4a6b986f24
Stefan Ghiata <stefanghiata@nn.nl>
2019-01-21 22:40:47 +0100

    pretty-print temperature and humidity values

--- sensor.py
+++ sensor.py
@@ -9,6 +9,6 @@ pin = 17
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None and temperature is not None:
-    print(temperature, humidity)
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
